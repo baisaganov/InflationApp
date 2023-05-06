49b40532edabf20477271d01785b1f0df1d95969
Alisher <baisaganov99@gmail.com>
2023-05-06 22:30:50 +0600

    Create project

---
+++ src/main/java/kz/inflation/InflationApp/InflationAppApplication.java
@@ -0,0 +1,13 @@
+package kz.inflation.InflationApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/inflation/InflationApp/InflationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.inflation.InflationApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
