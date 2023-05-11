98b05e6dbf9ae55258dc2c0d72ce5e20675c0fd6
Alisher <baisaganov99@gmail.com>
2023-05-12 00:24:12 +0600

    Added loading button
    Signed-off-by: Alisher <baisaganov99@gmail.com>

--- src/main/resources/static/js/listOfProducts.js
+++ src/main/resources/static/js/listOfProducts.js
@@ -1,23 +1,33 @@
 /* globals Chart:false, feather:false */
 
-(async () => {
-    'use strict'
+async function getData(page= 0){
 
     feather.replace({'aria-hidden': 'true'})
     // Data from server
-
-    let response = await fetch("http://localhost:8080/api/products/unique")
+    let loadingButton = document.querySelector('.counter')
+    loadingButton.innerHTML = '<button class="btn btn-primary" type="button" disabled>\n' +
+        '                <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>\n' +
+        '                Loading...\n' +
+        '            </button>'
+    let response = await fetch("http://localhost:8080/api/products/unique?page=" + page)
     let content = await response.json()
     // List
-    let prevPrice
     let list = document.querySelector('.productsList')
+    list.innerHTML = ""
     for (let key in content){
 
         list.innerHTML +=       '<tr>\n' +
             '                        <td>' + content[key].articul + '</td>\n' +
             '                        <td>' + content[key].name + '</td>\n' +
-            '                        <td>' + '-' + '</td>\n' +
-            '                        <td>' + <a href={products/content[key].articul}>Подробнее...</a>+ '</td>\n' +
+            '                        <td>' + content[key].updatedTime + '</td>\n' +
+
+            '                        <td>\n' +
+            '<a href="/products/' + content[key].articul + '" class="btn btn-sm btn-ouclassName-secondary">Перейти</a></td>' +
             '                    </tr>'
     }
-})()
+    page+=1
+    loadingButton.innerHTML = '<button class="btn btn-primary d-inline-flex align-items-right" onclick="getData(' + page + ')" type="button">\n' +
+        '    Далее →' +
+        '<script src="/js/listOfProducts.js"></script>' +
+        '  </button>'
+}
