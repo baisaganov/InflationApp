c3d01026778fdd1d269ca4523caaa301ba81cd89
Alisher <baisaganov99@gmail.com>
2023-05-16 00:38:29 +0600

    Добавлены еще категории, а также ожидание завершения потока
    Signed-off-by: Alisher <baisaganov99@gmail.com>

--- src/main/java/kz/inflation/InflationApp/scripts/ProductsParser.java
+++ src/main/java/kz/inflation/InflationApp/scripts/ProductsParser.java
@@ -9,14 +9,11 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 
 /*
-    TODO: Сделать многопоточный запуск
+    TODO: Поставить таймер на выполнение потока
 */
 @Component
 public class ProductsParser {
@@ -31,23 +28,46 @@ public class ProductsParser {
     }
     @Scheduled(initialDelay = 10000, fixedDelay = 1000 * 60 * 60 )
     public void startParsingProducts(){
-        ExecutorService service = Executors.newFixedThreadPool(6);
+        ExecutorService service = Executors.newFixedThreadPool(1);
 
-//        String milkLink = "https://kaspi.kz/shop/nur-sultan/c/dairy%20and%20eggs/?q=%3Acategory%3ADairy%20and%20eggs%3AallMerchants%3AMagnum&sort=relevance&sc=";
-//        String candies = "https://kaspi.kz/shop/nur-sultan/c/pastry/?q=%3Acategory%3APastry%3AallMerchants%3AMagnum&sort=relevance&sc=";
+        String milkLink = "https://kaspi.kz/shop/nur-sultan/c/dairy%20and%20eggs/?q=%3Acategory%3ADairy%20and%20eggs%3AallMerchants%3AMagnum&sort=relevance&sc=";
+        String candies = "https://kaspi.kz/shop/nur-sultan/c/pastry/?q=%3Acategory%3APastry%3AallMerchants%3AMagnum&sort=relevance&sc=";
         String fruitsAndVeg = "https://kaspi.kz/shop/nur-sultan/c/fruits%20and%20vegetables/?q=%3Acategory%3AFruits%20and%20vegetables%3AallMerchants%3AMagnum&sort=relevance&sc=";
         String juicesAndWater = "https://kaspi.kz/shop/nur-sultan/c/water%20and%20beverages/?q=%3Acategory%3AWater%20and%20beverages%3AallMerchants%3AMagnum&sort=relevance&sc=";
-//        String pasta = "https://kaspi.kz/shop/nur-sultan/c/grains%20and%20pasta/?q=%3Acategory%3AGrains%20and%20pasta%3AallMerchants%3AMagnum&sort=relevance&sc=";
-//        String oilsAndSauces = "https://kaspi.kz/shop/nur-sultan/c/spices%20and%20seasoning/?q=%3Acategory%3ASpices%20and%20seasoning%3AallMerchants%3AMagnum&sort=relevance&sc=";
+        String pasta = "https://kaspi.kz/shop/nur-sultan/c/grains%20and%20pasta/?q=%3Acategory%3AGrains%20and%20pasta%3AallMerchants%3AMagnum&sort=relevance&sc=";
+        String oilsAndSauces = "https://kaspi.kz/shop/nur-sultan/c/spices%20and%20seasoning/?q=%3Acategory%3ASpices%20and%20seasoning%3AallMerchants%3AMagnum&sort=relevance&sc=";
+        String bread = "https://kaspi.kz/shop/nur-sultan/c/bread%20and%20bakery/?q=%3Acategory%3ABread%20and%20bakery%3AallMerchants%3AMagnum&sort=relevance&sc=";
+        String sugarAndSpices = "https://kaspi.kz/shop/nur-sultan/c/sugar%20salt%20spices/?q=%3Acategory%3ASugar%20salt%20spices%3AallMerchants%3AMagnum&sort=relevance&sc=";
+        String cannedGoods = "https://kaspi.kz/shop/nur-sultan/c/canned%20goods/?q=%3Acategory%3ACanned%20goods%3AallMerchants%3AMagnum&sort=relevance&sc=";
+        String meatAndPoultry = "https://kaspi.kz/shop/nur-sultan/c/meat%20and%20poultry/?q=%3Acategory%3AMeat%20and%20poultry%3AallMerchants%3AMagnum&sort=relevance&sc=";
+        String sausages = "https://kaspi.kz/shop/nur-sultan/c/sausages%20and%20meat%20delicacies/?q=%3Acategory%3ASausages%20and%20meat%20delicacies%3AallMerchants%3AMagnum&sort=relevance&sc=";
+        String teaAndCoffee = "https://kaspi.kz/shop/nur-sultan/c/tea%20and%20coffee/?q=%3Acategory%3ATea%20and%20coffee%3AallMerchants%3AMagnum&sort=relevance&sc=";
+        String chipsAndNuts = "https://kaspi.kz/shop/nur-sultan/c/chips%20and%20nuts/?q=%3Acategory%3AChips%20and%20nuts%3AallMerchants%3AMagnum&sort=relevance&sc=";
+        String baking = "https://kaspi.kz/shop/nur-sultan/c/everything%20for%20baking/?q=%3Acategory%3AEverything%20for%20baking%3AallMerchants%3AMagnum&sort=relevance&sc=";
+        String frozenFoods = "https://kaspi.kz/shop/nur-sultan/c/frozen%20foods/?q=%3Acategory%3AFrozen%20foods%3AallMerchants%3AMagnum&sort=relevance&sc=";
+        String readyMeal = "https://kaspi.kz/shop/nur-sultan/c/ready%20meal/?q=%3Acategory%3AReady%20meal%3AallMerchants%3AMagnum&sort=relevance&sc=";
+        String seafood = "https://kaspi.kz/shop/nur-sultan/c/seafood/?q=%3Acategory%3ASeafood%3AallMerchants%3AMagnum&sort=relevance&sc=";
         List<Future<?>> futureList = new ArrayList<>();
+
         List<ThreadParser> parserList = new ArrayList<ThreadParser>(
                 List.of(
-//                        new ThreadParser(productService, milkLink, "milks"),
-//                        new ThreadParser(productService, candies,"candies"),
+                        new ThreadParser(productService, milkLink, "milks"),
+                        new ThreadParser(productService, candies,"candies"),
                         new ThreadParser(productService, fruitsAndVeg, "fruitsAndVeg"),
-                        new ThreadParser(productService, juicesAndWater, "juicesAndWater")
-//                        new ThreadParser(productService, pasta, "pasta"),
-//                        new ThreadParser(productService, oilsAndSauces, "oilsAndSauces")
+                        new ThreadParser(productService, juicesAndWater, "juicesAndWater"),
+                        new ThreadParser(productService, oilsAndSauces, "oilsAndSauces"),
+                        new ThreadParser(productService, pasta, "pasta"),
+                        new ThreadParser(productService, bread, "bread"),
+                        new ThreadParser(productService, sugarAndSpices, "sugarAndSpices"),
+                        new ThreadParser(productService, cannedGoods, "cannedGoods"),
+                        new ThreadParser(productService, meatAndPoultry, "meatAndPoultry"),
+                        new ThreadParser(productService, sausages, "sausages"),
+                        new ThreadParser(productService, teaAndCoffee, "teaAndCoffee"),
+                        new ThreadParser(productService, chipsAndNuts, "chipsAndNuts"),
+                        new ThreadParser(productService, baking, "baking"),
+                        new ThreadParser(productService, frozenFoods, "frozenFoods"),
+                        new ThreadParser(productService, readyMeal, "readyMeal"),
+                        new ThreadParser(productService, seafood, "seafood")
                 )
         );
 
@@ -65,12 +85,16 @@ public class ProductsParser {
             } catch (InterruptedException | ExecutionException e) {
                 throw new RuntimeException(e);
             }
-
         }
+
         service.shutdown();
+        try {
+            service.awaitTermination(2, TimeUnit.HOURS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        productService.saveNotUpdatedItems();
         productInflationService.updateData();
-
-
 
 
     }
