69af58f7fbbfbad3ec36506409343520162f44ff
Alisher <baisaganov99@gmail.com>
2023-05-16 00:41:48 +0600

    Добавлена сущность "категории"
    Signed-off-by: Alisher <baisaganov99@gmail.com>

---
+++ src/main/java/kz/inflation/InflationApp/models/ProductCategory.java
@@ -0,0 +1,60 @@
+package kz.inflation.InflationApp.models;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity(name = "product_category")
+public class ProductCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> product;
+
+    public ProductCategory() {
+    }
+
+    public ProductCategory(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Product> getProduct() {
+        return product;
+    }
+
+    public void setProduct(List<Product> product) {
+        this.product = product;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductCategory{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", product=" + product +
+                '}';
+    }
+}
